b29d6fa4929309e98ece6cc5528561434deecfcf
edsardgrisel <edsardgrisel1@gmail.com>
2024-08-09 11:59:21 +0200

    changed favouriteFolders to list

--- models/homePageModel.js
+++ models/homePageModel.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 const homePageSchema = new mongoose.Schema({
     photoUrl: { type: String, required: true },
-    featuredFolders: { type: String, required: true },
+    favouriteFolders: [{ type: String, required: true }],
 });
 
 module.exports = mongoose.model('HomePage', homePageSchema);
