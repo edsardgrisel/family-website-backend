ebc02721af3de1afd48422860c527fea73d3564e
edsardgrisel <edsardgrisel1@gmail.com>
2024-11-23 22:51:13 +0100

    handle frontend routing

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // Connect to MongoDB
 connectDB();
@@ -19,6 +19,15 @@ const folderRouter = require('./routes/folderRouter');
 app.use('/folders', folderRouter);
 const authRouter = require('./routes/authRouter');
 app.use('/auth', authRouter);
+
+// Serve React frontend
+const frontendPath = path.join(__dirname, 'frontend', 'build');
+app.use(express.static(frontendPath));
+
+// Catch-all route to serve React's index.html for SPA
+app.get('*', (req, res) => {
+    res.sendFile(path.join(frontendPath, 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
